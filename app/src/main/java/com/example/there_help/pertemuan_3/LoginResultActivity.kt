b7c49fbdaae91efefe27e875f57fb402dca6f759
whosaivilo <theresa24si@mahasiswa.pcr.ac.id>
2026-04-16 18:01:33 +0700

    buat intent

--- app/src/main/java/com/example/there_help/pertemuan_3/LoginResultActivity.kt
+++ app/src/main/java/com/example/there_help/pertemuan_3/LoginResultActivity.kt
@@ -1,31 +1,21 @@
 package com.example.there_help.pertemuan_3
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.there_help.databinding.ActivityLoginBinding
+import com.example.there_help.R
 
-class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+class LoginResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.btnlogin.setOnClickListener {
-            val intent = Intent(this, LoginResultActivity::class.java)
-            startActivity(intent)
-        }
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+        setContentView(R.layout.activity_login_result)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-
     }
 }
