afad0b7f6a09992e1cabb18fb72922be73b97698
whosaivilo <theresa24si@mahasiswa.pcr.ac.id>
2026-04-17 12:37:35 +0700

    perbaikan nama id

--- app/src/main/java/com/example/there_help/pertemuan_3/LoginResultActivity.kt
+++ app/src/main/java/com/example/there_help/pertemuan_3/LoginResultActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.there_help.R
+// Import otomatis untuk binding layout activity_login_result.xml
+import com.example.there_help.databinding.ActivityLoginResultBinding
 
 class LoginResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityLoginResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
